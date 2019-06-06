74b7fa1e8028838852e13c6c9887a3e40bc890c7
Manwoo Yi <navilera@gmail.com>
2019-06-05 23:02:53 -0700

    Implement string literal symbol table linked list

--- src/symtab.c
+++ src/symtab.c
@@ -22,4 +22,70 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
+#include <string.h>
+#include <stdlib.h>
 
+typedef struct _string_literal_tab_t_ {
+    char* string_literal_buffer;
+    struct _string_literal_tab_t_   *next;
+} string_literal_tab_t;
+
+static struct _string_literal_tab_linkedlist_t_ {
+    string_literal_tab_t*    head;
+    string_literal_tab_t*    tail;
+} s_string_literal_tab_linkedlist = {0};
+
+
+static string_literal_tab_t* find_string_literal(const char* string_literal);
+static string_literal_tab_t* insert_string_literal(const char* string_literal);
+
+
+char* Symtab_add_string_literal(const char* parser_str_ptr)
+{
+    string_literal_tab_t* string_literal_node = find_string_literal(parser_str_ptr);
+
+    if (string_literal_tab_t == NULL)
+    {
+        string_literal_node = insert_string_literal(parser_str_ptr);
+    }
+
+    return string_literal_node->string_literal_buffer;
+}
+
+static string_literal_tab_t* find_string_literal(const char* string_literal)
+{
+    string_literal_tab_t* iter = s_string_literal_tab_linkedlist.head;
+
+    while (iter != NULL)
+    {
+        if (strncmp(iter->string_literal_buffer, string_literal, strlen(string_literal)) == 0)
+        {
+            if (strlen(iter->string_literal_buffer) == strlen(string_literal))
+            {
+                return iter;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+static string_literal_tab_t* insert_string_literal(const char* string_literal)
+{
+    string_literal_tab_t* new_node = (string_literal_tab_t*)malloc(sizeof(string_literal_tab_t));
+
+    new_node->string_literal_buffer = strdup(string_literal);
+    new_node->next = NULL;
+
+    if (s_string_literal_tab_linkedlist.head != NULL)
+    {
+        s_string_literal_tab_linkedlist.head = new_node;
+        s_string_literal_tab_linkedlist.tail = s_string_literal_tab_linkedlist.head;
+    }
+    else
+    {
+        s_string_literal_tab_linkedlist.tail->next = new_node;
+    }
+
+    return new_node;
+}
