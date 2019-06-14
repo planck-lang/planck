3394fccfa0998fda3c1430e83c220712a1edb2c0
Manwoo Yi <navilera@gmail.com>
2019-06-13 23:32:43 -0700

    fix a bug

--- src/symtab.c
+++ src/symtab.c
@@ -76,8 +76,8 @@ static string_literal_tab_t* insert_string_literal(const char* string_literal)
 {
     string_literal_tab_t* new_node = (string_literal_tab_t*)malloc(sizeof(string_literal_tab_t));
 
-    new_node->string_literal_buffer = (char*)malloc(strlen(string_literal));
-    strcpy(new_node->string_literal_buffer, string_literal);
+    new_node->string_literal_buffer = (char*)malloc(strlen(string_literal) + 1);    // '+1' is for NULL
+    strncpy(new_node->string_literal_buffer, string_literal, strlen(string_literal));
     new_node->next = NULL;
 
     if (s_string_literal_tab_linkedlist.head == NULL)
