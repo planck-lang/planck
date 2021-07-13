277d4e0e66b62dc5f6ecaba39e780ae2bacaacea
Manwoo Yi <navilera@gmail.com>
2021-07-13 03:13:35 -0700

    opcode design

--- src/vm.c
+++ src/vm.c
@@ -3,9 +3,11 @@
 
 #include "vm.h"
 
+#define INC_PC()    g_Regs.pc += LEN_WORD
+#define INC_SP(N)   g_Regs.sp += (LEN_WORD * N)
+
 Reg_s_t g_Regs = {0};
 Mem_s_t g_Mem = {0};
-
 
 static void _assign_more_mem(Virtual_mem_s_t *vmem)
 {
@@ -18,4 +20,56 @@ void vm_init(void)
     _assign_more_mem(&g_Mem.data);
     _assign_more_mem(&g_Mem.inst);
     _assign_more_mem(&g_Mem.stack);
+}
+
+uint64_t vm_fetch(void)
+{
+    return (g_Mem.inst.mem[g_Regs.pc]);
+}
+
+void vm_decode(uint64_t op_bin)
+{
+    Opcode_u_t opcode = {0};
+    opcode.u64 = op_bin;
+
+    Inst_e_t inst = (Inst_e_t)opcode.instruction;
+
+    switch (inst)
+    {
+        case Inst_Nop:
+        break;
+
+        case Inst_Mov:
+        break;
+
+        case Inst_Ldr:
+        case Inst_Str:
+        break;
+
+        case Inst_Jmp:
+        case Inst_Brn:
+        break;
+
+        case Inst_Ret:
+        break;
+
+        case Inst_Push:
+        case Inst_Pop:
+        break;
+
+        case Inst_Add:
+        case Inst_Sub:
+        case Inst_Mul:
+        case Inst_Div:
+        case Inst_Mod:
+        case Inst_Shr:
+        case Inst_Shl:
+        case Inst_And:
+        case Inst_Or:
+        case Inst_Xor:
+        break;
+
+        default:
+         // Error
+    }
 }
--- src/vm.h
+++ src/vm.h
@@ -31,6 +31,7 @@ SOFTWARE.
 
 #pragma once
 
+#define LEN_WORD            sizeof(int64_t)
 #define NUM_REGS            64          // number of general registers
 #define GRAN_MEM            4096        // garanularity of the memory size as byte
 
@@ -42,18 +43,144 @@ typedef enum _inst_e_
     Inst_Str,
     Inst_Jmp,
     Inst_Brn,
+    Inst_Ret,
     Inst_Push,
     Inst_Pop,
-    Inst_Add = 0x100,
+    Inst_Add = 0x80,
     Inst_Sub,
     Inst_Mul,
     Inst_Div,
     Inst_Mod,
     Inst_Shr,
     Inst_Shl,
+    Inst_And,
+    Inst_Or,
+    Inst_Xor,
 } Inst_e_t;
 
-typedef union _status_reg_s_t_
+typedef enum _condition_e_
+{
+    Cond_None = 0,
+    Cond_Eq   = 1,
+    Cond_Neq  = 2,
+    Cond_gt   = 3,
+    Cond_lt   = 4,
+    Cond_get  = 5,
+    Cond_let  = 6,
+} Cond_e_t;
+
+typedef union _opcode_u_
+{
+    uint64_t u64;
+    uint8_t  instruction;
+    
+    union
+    {
+        /*
+        Simple type
+            * Source from register
+            +-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-+
+            |Instruction 8b | Dest Reg 8b   |0| reserved 7b |  Src Reg 8b   |                   Reserved 32b                                |
+
+            * Source from immediate value
+            +-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-+
+            |Instruction 8b | Dest Reg 8b   |1| reserved 7b |            Immediate Value 32b                                | reserved 8b   |
+        */
+        struct
+        {
+            uint8_t inst;
+            uint8_t dest_reg_id;
+            uint8_t is_imme;        // upper 7b are reserved
+            union
+            {
+                struct
+                {
+                    uint8_t id;
+                    uint32_t rsvd;
+                } reg;      // is_imm = 0
+
+                struct
+                {
+                    uint32_t value;
+                    uint8_t rsvd;
+                } imm;      // is_imm = 1
+            } src;
+        } simple_type;  // mov
+
+        /*
+        Arithmetic type
+            * reg = reg OP reg
+            +-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-+
+            |Instruction 8b | Dest Reg 8b   |0 0|  rsvd 6b  |    Reg 0      |      Reg 1    |          Reserved 24b                         |
+
+            * reg = reg OP imm
+            +-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-+
+            |Instruction 8b | Dest Reg 8b   |0 1|  rsvd 6b  |    Reg 0      |           Immediate Value 32b                                 |
+            
+            * reg = imm OP imm
+            +-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-*-+-+-+-+-+-+-+-+
+            |Instruction 8b | Dest Reg 8b   |1 0|  rsvd 6b  |    Immediate Value 0  16b     |    Immediate Value 1   16b    | reserved 8b   |
+        */
+       struct
+       {
+           uint8_t inst;
+           uint8_t dest_reg_id;
+           uint8_t operand_type;    // upper 6b are reserved
+           union
+           {
+               struct
+               {
+                   uint8_t reg0_id;
+                   uint8_t reg1_id;
+                   uint8_t rsvd[3];
+               } reg_reg;       // operand_type = 00b
+
+               struct
+               {
+                   uint8_t reg0_id;
+                   uint32_t imm_val;
+               } reg_imm;       // operand_type = 01b
+
+               struct
+               {
+                   uint16_t imm0_val;
+                   uint16_t imm1_val;
+                   uint8_t rsvd;
+               } imm_imm;       // operand_type = 10b
+           } operand;
+       } arithmetic_type;   // add, sub, mul ..... and, or, xor
+
+       /*
+       Jump type
+            * Register based PC relative addressing
+            * Immediate PC relative addressing
+            * Register based addressing
+            * Immediate addressing
+       */
+      struct
+      {
+
+      } jump_type;      // jmp, brn
+
+      /*
+      Memory type
+            * Store : [reg] = reg
+            * Store : [reg] = imm
+            * Store : [imm] = reg
+            * Store : [imm] = imm
+            * Load  : reg = [reg]
+            * Load  : reg = [imm]
+      */
+
+     /*
+     Stack type
+            * Push : [stack .....] = reg0, reg1 .... reg56
+            * Pop : reg0, reg1 .... reg56 = [stack .....]
+     */
+    } bytes;
+} Opcode_u_t;
+
+typedef union _status_reg_u_
 {
     uint64_t u64;
 
@@ -68,15 +195,16 @@ typedef union _status_reg_s_t_
         } u8_0;
 
         uint8_t reserved[7];
-    } byte_reg;
-} Stsr_s_t;
+    } bytes;
+} Stsr_u_t;
 
 typedef struct _reg_s_
 {
     uint64_t pc;
     uint64_t sp;
     uint64_t fp;
-    Stsr_s_t status;
+    uint64_t ld;
+    Stsr_u_t status;
     uint64_t r[NUM_REGS];
 } Reg_s_t;
 
@@ -95,5 +223,6 @@ typedef struct _mem_s_
 } Mem_s_t;
 
 
-void vm_init(void);
-void vm_execute(uint64_t opcode);
+void        vm_init(void);
+uint64_t    vm_fetch(void);
+void        vm_decode(uint64_t op_bin);
